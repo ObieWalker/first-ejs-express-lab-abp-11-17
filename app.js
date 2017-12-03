885dcd2c1567dcc1e9073409b9d87611922311d1
Obi <obinnawalker@gmail.com>
2017-12-03 17:16:45 +0000

    automatically backed up by learn

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express')
 var app = express()
+const path = require('path')
 
 // Build Your Route Here
 app.set('views', path.join(__dirname, 'views'));
@@ -7,7 +8,7 @@ app.set('view engine', 'ejs');
 
 app.get('/', function(req, resp){
   resp.render('index')
-  
+
 })
 
 module.exports = app
